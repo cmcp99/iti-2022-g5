e38240363829b30a268ca644f98127550be9d083
albanomelo1 <58079625+albanomelo1@users.noreply.github.com>
2022-09-28 19:11:27 +0100

    albano

--- server.py
+++ server.py
@@ -1,17 +1,20 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fpdf import FPDF
+from nacl.signing import SigningKey
 import os
 
 app = FastAPI()
+signing_key = SigningKey.generate()
 
-@app.get("/create")
-def createFile():
+@app.get("/create/{word}")
+def createFile(word: str):
+    signed = signing_key.sign(bytes(word, 'utf-8'))
     pdf = FPDF(orientation='L', unit='mm', format='A4')
     pdf.add_page()
     pdf.set_font("helvetica", "B", 140)
     for i in range(0,20):
-        pdf.cell(0, 150, "Hello World!", ln=True, align='C')
+        pdf.cell(0, 150, str(signed), ln=True, align='C')
     i = int(lastFile()) + 1
     pdf.output(str(i) + ".pdf") 
     return "Ficheiro criado com sucesso!"
@@ -21,8 +24,9 @@ def lastFile():
     files = []
     for i in os.listdir():
             if i.endswith('.pdf'):
-                files.append(i)
-    return files[-1].replace('.pdf', '')
+                files.append(int(i.replace('.pdf', '')))
+    files.sort()
+    return files[-1]
 
 @app.get("/files")
 def getFiles():
