de2067a540d074d51b2a85b0765fb47062c39c05
albanomelo1 <albano.jaime@hotmail.com>
2022-12-20 17:23:37 +0000

    asdas

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,2 @@
-# 
-FROM python:3.9
-
-# 
-WORKDIR /iti-2022-g5
-
-# 
-COPY ./requirements.txt .
-
-# 
-RUN pip install --no-cache-dir --upgrade -r ./requirements.txt
-
-# 
-COPY ./server.py .
-
-# 
-CMD ["uvicorn", "server:app", "--host", "0.0.0.0", "--port", "80"]
+FROM nginx
+COPY nginx.conf /etc/nginx/nginx.conf
---
+++ app/server.py
@@ -0,0 +1,92 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fpdf import FPDF
+from nacl.signing import SigningKey
+import os
+import time
+
+""" mkdir /mnt/mountedshare
+mount -t drvfs '\\servername\sharename' /mnt/mountedshare """
+
+#linux /mnt/mountedshare/
+#windows //192.168.1.36/smb01/
+
+dir = 'Ficheiros/' 
+app = FastAPI()
+signing_key = SigningKey.generate()
+
+# Eliminar ficheiros .pdf ao iniciar o servidor
+@app.on_event("startup")
+def start_up():
+    for i in os.listdir(dir):
+        if i.endswith('.pdf'):
+            os.remove(dir + i)
+
+#MUDAR PARA POST
+@app.get("/create/{word}")
+def createFile(word: str):
+    a = time.time()
+    pdf = FPDF(orientation='L', unit='mm', format='A4')
+    pdf.add_page()
+    pdf.set_font("helvetica", "B", 45)
+    while pdf.page_no() != 100:
+        signed = signing_key.sign(bytes(word, 'utf-8'))
+        pdf.cell(80, 10, str(signed), ln=True, align='C')
+    b = time.time()
+    print('Tempo para preencher PDF: ', b-a)
+    c = time.time()
+    i = int(lastFile()) + 1
+    pdf.output(dir + str(i) + ".pdf")
+    d = time.time()
+    print('Tempo para escrita no disco: ', d-c)
+    return "Ficheiro criado com sucesso!"
+
+@app.post("/createn/{word}/{pages}")
+def createFile(word: str, pages: int):
+    a = time.time()
+    pdf = FPDF(orientation='L', unit='mm', format='A4')
+    pdf.add_page()
+    pdf.set_font("helvetica", "B", 45)
+    while pdf.page_no() != pages:
+        signed = signing_key.sign(bytes(word, 'utf-8'))
+        pdf.cell(80, 10, str(signed), ln=True, align='C')
+    b = time.time()
+    print('Tempo para preencher PDF: ', b-a)
+    c = time.time()
+    i = int(lastFile()) + 1
+    pdf.output(dir + str(i) + ".pdf")
+    d = time.time()
+    print('Tempo para escrita no disco: ', d-c)
+    return "Ficheiro criado com sucesso!"
+
+# procura id do ultimo ficheiro adicionado
+def lastFile():
+    files = []
+    for i in os.listdir(dir):
+        if i.endswith('.pdf'):
+            files.append(int(i.replace('.pdf', '')))
+    files.sort()
+    if files == []:
+        return 0
+    else:
+        return files[-1]
+
+@app.get("/files")
+def getFiles():
+    a = time.time()
+    files=[]
+    for i in os.listdir(dir):
+        if i.endswith('.pdf'):
+            files.append(i)
+    b = time.time()
+    print('Tempo de acesso a todos os ficheiros no disco: ', b-a)
+    return files
+
+@app.get("/file/{name}")
+def sendFile(name: str):
+    a = time.time()
+    b = FileResponse(dir + name)
+    c = time.time()
+    print('Tempo de leitura do ficheiro no disco: ', c-a)
+    return b
+    
